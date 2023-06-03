029cc56251def1c7a40f890dc1310ac16915848a
RAguirreGiraldo <raguirregiraldo@gmail.com>
2023-06-02 22:39:39 -0500

    Create components Body, BodyContent and Update CSS

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import Banner from './Banner';
+import BodyContent from './BodyContent';
 
 function Body() {
     return(
-        <div className='body-container'>
-            <div className="banner-container">
-                <Banner message = "Enjoy Our galery"/>
-            </div>            
-            <div className='body-content'>
-
+        <div className="banner-container">
+            <div className="banner">
+                <Banner message="Enjoy Our gallery" />
             </div>
-        </div>
+            <div className="body-container">
+                <div className="body-content">
+                    <BodyContent />
+                </div>
+            </div>
+            </div>        
     );
 }
 
 export default Body;
+
+/* 
+        <div className="banner-container">
+            <div className="banner">
+                <Banner message = "Enjoy Our galery"/>
+            </div>            
+            <div className="content-container">
+                <BodyContent />
+            </div>
+        </div>*/ 
---
+++ src/components/BodyContent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function BodyContent() {
+    return(
+        <div className="body-content">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipiscing elit. 
+                Sed accumsan neque vitae odio porttitor commodo. Cras venenatis sollicitudin turpis, 
+                eu mollis justo porttitor quis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+        </div>
+    );
+}
+
+export default BodyContent;
